4683af5163557b564325600e3523eaf560a5b8fa
thomasreichmann <thomasalmeidar@gmail.com>
2023-08-01 14:01:32 -0300

    base

---
+++ src/test/java/dev/thomasar/samsungevaluationapi/SamsungEvaluationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thomasar.samsungevaluationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsungEvaluationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
