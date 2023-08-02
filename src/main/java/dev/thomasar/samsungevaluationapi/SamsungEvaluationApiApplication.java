657515618372301ca8818b9c66ffa86a7d630d14
thomasreichmann <thomasalmeidar@gmail.com>
2023-08-01 23:11:18 -0300

    cors config

--- src/main/java/dev/thomasar/samsungevaluationapi/SamsungEvaluationApiApplication.java
+++ src/main/java/dev/thomasar/samsungevaluationapi/SamsungEvaluationApiApplication.java
@@ -3,6 +3,9 @@ package dev.thomasar.samsungevaluationapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +15,14 @@ public class SamsungEvaluationApiApplication {
         SpringApplication.run(SamsungEvaluationApiApplication.class, args);
     }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+            }
+        };
+    }
 }
